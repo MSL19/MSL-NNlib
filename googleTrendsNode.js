92c40d1db8f4f0ed1032c60fcef686500da37db5
MSL19 <40368820+MSL19@users.noreply.github.com>
2018-10-21 15:15:30 -0600

    Figured out how to pull data from google trends
    using the google-trends-api

---
+++ googleTrendsNode.js
@@ -0,0 +1,8 @@
+const googleTrends = require('google-trends-api');
+googleTrends.relatedTopics({keyword: 'Apple', startTime: new Date('2018-10-19'), endTime: new Date(Date.now())})
+.then((res) => {
+  console.log(res);
+})
+.catch((err) => {
+  console.log(err);
+})
