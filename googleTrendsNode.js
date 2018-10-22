7456aa0cc63ff0c9996bfd590edf066e680478d7
MSL19 <40368820+MSL19@users.noreply.github.com>
2018-10-22 11:21:57 -0600

    Revert "Figured out how to pull data from google trends"
    This reverts commit 92c40d1db8f4f0ed1032c60fcef686500da37db5.

--- googleTrendsNode.js
+++
@@ -1,8 +0,0 @@
-const googleTrends = require('google-trends-api');
-googleTrends.relatedTopics({keyword: 'Apple', startTime: new Date('2018-10-19'), endTime: new Date(Date.now())})
-.then((res) => {
-  console.log(res);
-})
-.catch((err) => {
-  console.log(err);
-})
