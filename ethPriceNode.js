126a6fec085943cf6b430cb6a316d064b4bf3015
MSL19 <40368820+MSL19@users.noreply.github.com>
2018-11-12 12:40:47 -0700

    even worse
    asdasdas

--- ethPriceNode.js
+++ ethPriceNode.js
@@ -38,16 +38,6 @@ async function predictPrice(){
     console.log(currentPrice);
     console.log(currentVolume);
     console.log(currentIntrest);
-<<<<<<< HEAD
-    var today = new Date();
-    var h = today.getHours();
-    var m = today.getMinutes();
-    var s = today.getSeconds();
-    
-    console.log("Time: "+h+":"+m+":"+s);
-   
-=======
->>>>>>> parent of d3331dd... Ready to work on the whole problem of hosting this garbage on a server somewhere.....
 
     let priceDelta = (currentPrice-previousPrice)/previousPrice;
     console.log("Price delta percent: "+priceDelta);
@@ -167,11 +157,7 @@ var https = require("https");
                                                        
 
 
-<<<<<<< HEAD
-setInterval(updateNN, 1000);
-=======
 setInterval(updateNN, 20000);
->>>>>>> parent of d3331dd... Ready to work on the whole problem of hosting this garbage on a server somewhere.....
 //getPrice();
 var price;
 var pastPrice;
